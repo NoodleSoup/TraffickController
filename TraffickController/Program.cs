f53ecd4b38e518925134b349b6a52559f280d416
Daan <eekhof.daan@gmail.com>
2020-03-26 15:38:37 +0100

    Program implement console arguments for setting the request path

--- TraffickController/Program.cs
+++ TraffickController/Program.cs
@@ -1,17 +1,58 @@
-﻿using Microsoft.AspNetCore;
+﻿using CommandLine;
+using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace TrafficController
+namespace TraffickController
 {
     public class Program
     {
+        public static string pathUrl;
         public static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments passed");
+            }
+            else
+            {
+                try
+                {
+                    // Parse the command line variables.
+                    Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+                    {
+                        setPathUrl(o.path.ToString()); // Moet nog veranderd worden voor de juiste shizzle
+                    });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e);
+                }
+            }
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static string setPathUrl(string path)
+        {
+            pathUrl = path;
+            return "";
+        }
+
+        public static string getPathUrl()
+        {
+            if(!String.IsNullOrEmpty(pathUrl))
+                return pathUrl;
+            return "";
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
               .UseStartup<Startup>();
+    }
+
+    internal class Options
+    {
+        [Option('p', "path", Required = true, HelpText = "Set path ding.")]
+        public string path { get; set; }
     }
 }
