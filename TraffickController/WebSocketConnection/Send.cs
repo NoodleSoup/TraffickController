e4f348eb752d1b72ba34e64fe65457c6b78a2a2a
Daan <eekhof.daan@gmail.com>
2020-03-27 18:11:13 +0100

    Added send functions to their respective folders

---
+++ TraffickController/WebSocketConnection/Send.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using TraffickController.JsonStrings;
+
+namespace TraffickController.WebSocketConnection
+{
+    public class Send
+    {
+        #region SendState
+        public static async Task SendState(HttpContext context, WebSocket webSocket)
+        {
+            var jsonTrafficLight = JsonStringBuilder.BuildJsonString(); // Start setting up the JSON string builder TODO: Make it send dynamic states
+            var jsonBytes = Encoding.UTF8.GetBytes(jsonTrafficLight); // Convert the JSON object to bytes to send over WebSocket connection
+
+            await webSocket.SendAsync(new ArraySegment<byte>(jsonBytes, 0, jsonBytes.Length), 0, true, CancellationToken.None);
+        }
+        #endregion
+
+        #region SendStartState
+        public static async Task<bool> SendStartState(HttpContext context, WebSocket webSocket)
+        {
+            var jsonTrafficLight = JsonStringBuilder.BuildJsonString();
+            var jsonBytes = Encoding.UTF8.GetBytes(jsonTrafficLight);
+
+            await webSocket.SendAsync(new ArraySegment<byte>(jsonBytes, 0, jsonBytes.Length), 0, true, CancellationToken.None);
+            return false;
+        }
+        #endregion
+    }
+}
