e392c73d9fa7af5453bf2888139204ca8137aa04
Daan <eekhof.daan@gmail.com>
2020-03-27 18:11:32 +0100

    Added receive functions to their respective folder

---
+++ TraffickController/WebSocketConnection/Receive.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TraffickController.WebSocketConnection
+{
+    public class Receive
+    {
+        #region Receive
+        public static async Task<bool> ReceiveSocket(HttpContext context, WebSocket webSocket, byte[] buffer)
+        {
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            return false;
+        }
+        #endregion
+    }
+}
