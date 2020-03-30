b48a09572979b4461e412311e09360203917744c
Daan <eekhof.daan@gmail.com>
2020-03-30 13:43:50 +0200

    Added converting Json string to object

--- TraffickController/WebSocketConnection/Receive.cs
+++ TraffickController/WebSocketConnection/Receive.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.Linq;
 using System.Net.WebSockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using TraffickController.JsonStrings;
 
 namespace TraffickController.WebSocketConnection
 {
@@ -12,10 +17,20 @@ namespace TraffickController.WebSocketConnection
         public static async Task<bool> ReceiveSocket(HttpContext context, WebSocket webSocket, byte[] buffer)
         {
             WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            byte[] msgBytes = buffer.Take(result.Count).ToArray();
+            string rcvMsg = Encoding.UTF8.GetString(msgBytes);
+            Console.WriteLine("{0}", rcvMsg);
+
+            var jsonObject = JsonConvert.DeserializeObject<TrafficLightObject>(rcvMsg);
+            Console.WriteLine(jsonObject.A1);
+
             while (!result.CloseStatus.HasValue)
             {
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                Console.WriteLine(result.Count);
             }
+
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
             return false;
         }
