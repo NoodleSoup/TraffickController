32377cc4b94d48a44370d82ba6547061c7391c5f
Daan <eekhof.daan@gmail.com>
2020-03-30 13:43:21 +0200

    Simplified string builder & added default values in constructor to overwrite certain values

--- TraffickController/JsonStrings/JsonStringBuilder.cs
+++ TraffickController/JsonStrings/JsonStringBuilder.cs
@@ -1,94 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace TraffickController.JsonStrings
 {
     public class JsonStringBuilder
     {
         #region BuildString
-        public static string BuildJsonString()
+        public static string BuildJsonString(int A1 = 0, int A2 = 0, int A3 = 0, int A4 = 0, int AB1 = 0, int AB2 = 0,
+                                             int B1 = 0, int B2 = 0, int B3 = 0, int B4 = 0, int B5 = 0, int BB1 = 0,
+                                             int C1 = 0, int C2 = 0, int C3 = 0, int D1 = 0, int D2 = 0, int D3 = 0,
+                                             int E1 = 0, int E2 = 0, int EV1 = 0, int EV2 = 0, int EV3 = 0, int EV4 = 0,
+                                             int FF1 = 0, int FF2 = 0, int FV1 = 0, int FV2 = 0, int FV3 = 0,
+                                             int FV4 = 0, int GF1 = 0, int GF2 = 0, int GV1 = 0, int GV2 = 0,
+                                             int GV3 = 0, int GV4 = 0)
         {
-            var trafficLights = new TrafficLightObject();
             // All traffic lights, can have a value of [0, 1, 2]
-            trafficLights.A1 = 0;
-            trafficLights.A2 = 0;
-            trafficLights.A3 = 0;
-            trafficLights.A4 = 0;
-            trafficLights.AB1 = 0;
-            trafficLights.AB2 = 0;
-            trafficLights.B1 = 0;
-            trafficLights.B2 = 0;
-            trafficLights.B3 = 0;
-            trafficLights.B4 = 0;
-            trafficLights.B5 = 0;
-            trafficLights.BB1 = 0;
-            trafficLights.C1 = 0;
-            trafficLights.C2 = 0;
-            trafficLights.C3 = 0;
-            trafficLights.D1 = 0;
-            trafficLights.D2 = 0;
-            trafficLights.D3 = 0;
-            trafficLights.E1 = 0;
-            trafficLights.E2 = 0;
-            trafficLights.EV1 = 0;
-            trafficLights.EV2 = 0;
-            trafficLights.EV3 = 0;
-            trafficLights.EV4 = 0;
-            trafficLights.FF1 = 0;
-            trafficLights.FF2 = 0;
-            trafficLights.FV1 = 0;
-            trafficLights.FV2 = 0;
-            trafficLights.FV3 = 0;
-            trafficLights.FV4 = 0;
-            trafficLights.GF1 = 0;
-            trafficLights.GF2 = 0;
-            trafficLights.GV1 = 0;
-            trafficLights.GV2 = 0;
-            trafficLights.GV3 = 0;
-            trafficLights.GV4 = 0;
-
-            var jsonString = $@"
-            {{
-                ""A1"": {trafficLights.A1},
-                ""A2"": {trafficLights.A2},
-                ""A3"": {trafficLights.A3},
-                ""A4"": {trafficLights.A4},
-                ""AB1"": {trafficLights.AB1},
-                ""AB2"": {trafficLights.AB2},
-                ""B1"": {trafficLights.B1},
-                ""B2"": {trafficLights.B2},
-                ""B3"": {trafficLights.B3},
-                ""B4"": {trafficLights.B4},
-                ""B5"": {trafficLights.B5},
-                ""BB1"": {trafficLights.BB1},
-                ""C1"": {trafficLights.C1},
-                ""C2"": {trafficLights.C2},
-                ""C3"": {trafficLights.C3},
-                ""D1"": {trafficLights.D1},
-                ""D2"": {trafficLights.D2},
-                ""D3"": {trafficLights.D3},
-                ""E1"": {trafficLights.E1},
-                ""E2"": {trafficLights.E2},
-                ""EV1"": {trafficLights.EV1},
-                ""EV2"": {trafficLights.EV2},
-                ""EV3"": {trafficLights.EV3},
-                ""EV4"": {trafficLights.EV4},
-                ""FF1"": {trafficLights.FF1},
-                ""FF2"": {trafficLights.FF2},
-                ""FV1"": {trafficLights.FV1},
-                ""FV2"": {trafficLights.FV2},
-                ""FV3"": {trafficLights.FV3},
-                ""FV4"": {trafficLights.FV4},
-                ""GF1"": {trafficLights.GF1},
-                ""GF2"": {trafficLights.GF2},
-                ""GV1"": {trafficLights.GV1},
-                ""GV2"": {trafficLights.GV2},
-                ""GV3"": {trafficLights.GV3},
-                ""GV4"": {trafficLights.GV4}
-            }}";
-
-            return jsonString;
+            var trafficLights = new TrafficLightObject()
+            {
+                A1 = A1,
+                A2 = A2,
+                A3 = A3,
+                A4 = A4,
+                AB1 = AB1,
+                AB2 = AB2,
+                B1 = B1,
+                B2 = B2,
+                B3 = B3,
+                B4 = B4,
+                B5 = B5,
+                BB1 = BB1,
+                C1 = C1,
+                C2 = C2,
+                C3 = C3,
+                D1 = D1,
+                D2 = D2,
+                D3 = D3,
+                E1 = E1,
+                E2 = E2,
+                EV1 = EV1,
+                EV2 = EV2,
+                EV3 = EV3,
+                EV4 = EV4,
+                FF1 = FF1,
+                FF2 = FF2,
+                FV1 = FV1,
+                FV2 = FV2,
+                FV3 = FV3,
+                FV4 = FV4,
+                GF1 = GF1,
+                GF2 = GF2,
+                GV1 = GV1,
+                GV2 = GV2,
+                GV3 = GV3,
+                GV4 = GV4
+            };
+            
+            return JsonConvert.SerializeObject(trafficLights);
         }
         #endregion
     }
