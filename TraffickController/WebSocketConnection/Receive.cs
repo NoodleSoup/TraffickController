2ebee89e973877849613fa843018ef3335333588
Daan <eekhof.daan@gmail.com>
2020-03-31 13:08:48 +0200

    [CHANGE] Changed close status

--- TraffickController/WebSocketConnection/Receive.cs
+++ TraffickController/WebSocketConnection/Receive.cs
@@ -20,19 +20,15 @@ namespace TraffickController.WebSocketConnection
 
             byte[] msgBytes = buffer.Take(result.Count).ToArray();
             string rcvMsg = Encoding.UTF8.GetString(msgBytes);
-            Console.WriteLine("{0}", rcvMsg);
 
             var jsonObject = JsonConvert.DeserializeObject<TrafficLightObject>(rcvMsg);
             Console.WriteLine(jsonObject.A1);
 
-            while (!result.CloseStatus.HasValue)
-            {
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                Console.WriteLine(result.Count);
-            }
-
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-            return false;
+            Console.WriteLine(result.CloseStatus.HasValue);
+            if (result.CloseStatus.HasValue)
+                return false;
+            
+            return true;
         }
         #endregion
     }
