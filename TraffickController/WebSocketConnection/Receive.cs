02afddeaccb5c13c0143ce5da277888c4bfa8786
Daan <eekhof.daan@gmail.com>
2020-03-31 20:51:34 +0200

    [ADD] Create updated TrafficLight Json string from received data

--- TraffickController/WebSocketConnection/Receive.cs
+++ TraffickController/WebSocketConnection/Receive.cs
@@ -13,6 +13,7 @@ namespace TraffickController.WebSocketConnection
 {
     public class Receive
     {
+        private static string _trafficLight;
         #region Receive
         public static async Task<bool> ReceiveSocket(HttpContext context, WebSocket webSocket, byte[] buffer)
         {
@@ -22,13 +23,43 @@ namespace TraffickController.WebSocketConnection
             string rcvMsg = Encoding.UTF8.GetString(msgBytes);
 
             var jsonObject = JsonConvert.DeserializeObject<TrafficLightObject>(rcvMsg);
-            Console.WriteLine(jsonObject.A1);
+            SetObjectData(jsonObject);
 
-            Console.WriteLine(result.CloseStatus.HasValue);
+            Thread.Sleep(500);
+
             if (result.CloseStatus.HasValue)
                 return false;
             
             return true;
+        }
+        #endregion
+
+        #region SetObjectData
+        private static void SetObjectData(TrafficLightObject trafficLightObject)
+        {
+            string newTrafficLight = JsonStringBuilder.BuildJsonString(
+                A1: trafficLightObject.A1, A2: trafficLightObject.A2, A3: trafficLightObject.A3,
+                A4: trafficLightObject.A4, AB1: trafficLightObject.AB1, AB2: trafficLightObject.AB2,
+                B1: trafficLightObject.B1, B2: trafficLightObject.B2, B3: trafficLightObject.B3,
+                B4: trafficLightObject.B4, B5: trafficLightObject.B5, BB1: trafficLightObject.BB1,
+                C1: trafficLightObject.C1, C2: trafficLightObject.C2, C3: trafficLightObject.C3,
+                D1: trafficLightObject.D1, D2: trafficLightObject.D2, D3: trafficLightObject.D3,
+                E1: trafficLightObject.E1, E2: trafficLightObject.E2, EV1: trafficLightObject.EV1,
+                EV2: trafficLightObject.EV2, EV3: trafficLightObject.EV3, EV4: trafficLightObject.EV4,
+                FF1: trafficLightObject.FF1, FF2: trafficLightObject.FF2, FV1: trafficLightObject.FV1,
+                FV2: trafficLightObject.FV2, FV3: trafficLightObject.FV3, FV4: trafficLightObject.FV4,
+                GF1: trafficLightObject.GF1, GF2: trafficLightObject.GF2, GV1: trafficLightObject.GV1,
+                GV2: trafficLightObject.GV2, GV3: trafficLightObject.GV3, GV4: trafficLightObject.GV4);
+
+            _trafficLight = newTrafficLight;
+
+        }
+        #endregion
+
+        #region GetNewTrafficLight
+        public static string GetNewTrafficLight()
+        {
+            return _trafficLight;
         }
         #endregion
     }
