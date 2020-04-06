f9cde281f112043e033166216962d304ea73d98d
Daan <eekhof.daan@gmail.com>
2020-04-06 12:07:00 +0200

    [CHANGE] Pushed the name change from April 1

--- TraffickController/JsonStrings/JsonStringBuilder.cs
+++ TraffickController/JsonStrings/JsonStringBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using TraffickController.TrafficLight;
 
 namespace TraffickController.JsonStrings
 {
@@ -8,7 +9,7 @@ namespace TraffickController.JsonStrings
         public static string BuildJsonString(int A1 = 0, int A2 = 0, int A3 = 0, int A4 = 0, int AB1 = 0, int AB2 = 0,
                                              int B1 = 0, int B2 = 0, int B3 = 0, int B4 = 0, int B5 = 0, int BB1 = 0,
                                              int C1 = 0, int C2 = 0, int C3 = 0, int D1 = 0, int D2 = 0, int D3 = 0,
-                                             int E1 = 0, int E2 = 0, int EV1 = 0, int EV2 = 0, int EV3 = 0, int EV4 = 0,
+                                             int E1 = 0, int EV1 = 0, int EV2 = 0, int EV3 = 0, int EV4 = 0,
                                              int FF1 = 0, int FF2 = 0, int FV1 = 0, int FV2 = 0, int FV3 = 0,
                                              int FV4 = 0, int GF1 = 0, int GF2 = 0, int GV1 = 0, int GV2 = 0,
                                              int GV3 = 0, int GV4 = 0)
@@ -35,7 +36,6 @@ namespace TraffickController.JsonStrings
                 D2 = D2,
                 D3 = D3,
                 E1 = E1,
-                E2 = E2,
                 EV1 = EV1,
                 EV2 = EV2,
                 EV3 = EV3,
