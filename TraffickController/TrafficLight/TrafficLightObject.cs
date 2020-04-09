176b25e8c785c381f741824a34cd5ec99b2eeb89
Daan <eekhof.daan@gmail.com>
2020-04-09 11:31:52 +0200

    [ADD] Set default values for TrafficLightObject

--- TraffickController/TrafficLight/TrafficLightObject.cs
+++ TraffickController/TrafficLight/TrafficLightObject.cs
@@ -2,40 +2,40 @@
 {
     public class TrafficLightObject
     {
-        public int A1 { get; set; }
-        public int A2 { get; set; }
-        public int A3 { get; set; }
-        public int A4 { get; set; }
-        public int AB1 { get; set; }
-        public int AB2 { get; set; }
-        public int B1 { get; set; }
-        public int B2 { get; set; }
-        public int B3 { get; set; }
-        public int B4 { get; set; }
-        public int B5 { get; set; }
-        public int BB1 { get; set; }
-        public int C1 { get; set; }
-        public int C2 { get; set; }
-        public int C3 { get; set; }
-        public int D1 { get; set; }
-        public int D2 { get; set; }
-        public int D3 { get; set; }
-        public int E1 { get; set; }
-        public int EV1 { get; set; }
-        public int EV2 { get; set; }
-        public int EV3 { get; set; }
-        public int EV4 { get; set; }
-        public int FF1 { get; set; }
-        public int FF2 { get; set; }
-        public int FV1 { get; set; }
-        public int FV2 { get; set; }
-        public int FV3 { get; set; }
-        public int FV4 { get; set; }
-        public int GF1 { get; set; }
-        public int GF2 { get; set; }
-        public int GV1 { get; set; }
-        public int GV2 { get; set; }
-        public int GV3 { get; set; }
-        public int GV4 { get; set; }
+        public int A1 { get; set; } = 0;
+        public int A2 { get; set; } = 0;
+        public int A3 { get; set; } = 0;
+        public int A4 { get; set; } = 0;
+        public int AB1 { get; set; } = 0;
+        public int AB2 { get; set; } = 0;
+        public int B1 { get; set; } = 0;
+        public int B2 { get; set; } = 0;
+        public int B3 { get; set; } = 0;
+        public int B4 { get; set; } = 0;
+        public int B5 { get; set; } = 0;
+        public int BB1 { get; set; } = 0;
+        public int C1 { get; set; } = 0;
+        public int C2 { get; set; } = 0;
+        public int C3 { get; set; } = 0;
+        public int D1 { get; set; } = 0;
+        public int D2 { get; set; } = 0;
+        public int D3 { get; set; } = 0;
+        public int E1 { get; set; } = 0;
+        public int EV1 { get; set; } = 0;
+        public int EV2 { get; set; } = 0;
+        public int EV3 { get; set; } = 0;
+        public int EV4 { get; set; } = 0;
+        public int FF1 { get; set; } = 0;
+        public int FF2 { get; set; } = 0;
+        public int FV1 { get; set; } = 0;
+        public int FV2 { get; set; } = 0;
+        public int FV3 { get; set; } = 0;
+        public int FV4 { get; set; } = 0;
+        public int GF1 { get; set; } = 0;
+        public int GF2 { get; set; } = 0;
+        public int GV1 { get; set; } = 0;
+        public int GV2 { get; set; } = 0;
+        public int GV3 { get; set; } = 0;
+        public int GV4 { get; set; } = 0;
     }
 }
