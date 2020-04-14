703e3267ebc0a4cf9e274decbef72f46263293f1
Daan <eekhof.daan@gmail.com>
2020-04-14 13:44:11 +0200

    [CHANGE] Send method states
    Changed how the server sends states to the controller, made a Thread.Sleep to send each state according to their time.
    Added a little note for a different solution depending on how often we need to send the states back to the client.

--- TraffickController/WebSocketConnection/Send.cs
+++ TraffickController/WebSocketConnection/Send.cs
@@ -12,18 +12,37 @@ namespace TraffickController.WebSocketConnection
 {
     public class Send
     {
+        private static string jsonTrafficLight = null;
         #region SendState
         public static async Task SendState(HttpContext context, WebSocket webSocket)
-        {            
-            string jsonTrafficLight = Data.GetNewTrafficLight(); // Start setting up the JSON string builder
+        {
+            jsonTrafficLight = Data.GetNewTrafficLight(); // Start setting up the JSON string builder
 
             // If the trafficlight string is empty for some reason send the default response (start state)
             if (String.IsNullOrEmpty(jsonTrafficLight))
                 jsonTrafficLight = JsonStringBuilder.BuildJsonString();
 
-            var jsonBytes = Encoding.UTF8.GetBytes(jsonTrafficLight); // Convert the JSON object to bytes to send over WebSocket connection
+            for (int x = 0; x < 3; x++){
 
-            await webSocket.SendAsync(new ArraySegment<byte>(jsonBytes, 0, jsonBytes.Length), 0, true, CancellationToken.None);
+                var jsonBytes = Encoding.UTF8.GetBytes(jsonTrafficLight); // Convert the JSON object to bytes to send over WebSocket connection
+
+                await webSocket.SendAsync(new ArraySegment<byte>(jsonBytes, 0, jsonBytes.Length), 0, true, CancellationToken.None);
+
+                Thread.Sleep(1000);
+            }
+
+            /* TODO: Think about this solution to see what is better
+            * The above just sends the same thing every second for 3 seconds long, the below sends it once
+            * and keeps the code from sending something else in those 3 seconds.
+
+            You could also use something like this:
+            send GreenState();
+            Thread.Sleep(6000); for the 6 seconds of greenlight decided by law
+            send OrangeState();
+            Thread.Sleep(2000); 2 seconds on orange
+            send RedState(); no sleep here as it isn't necessary
+            */
+
         }
         #endregion
 
